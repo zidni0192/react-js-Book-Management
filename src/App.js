aec819a6eef931d656d26c4a91f976b345e296b2
zidni0192 <izedeledby@gmail.com>
2019-07-02 09:26:56 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Nav from '../src/screen/Navbar'
+import List from '../src/screen/list'
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+  }
+  render() {
+    return (
+      <div id="app">
+        <Nav />
+
+        <form>
+          <input type="text" id="search" placeholder="Search Book ..." />
+        </form>
+        <List />
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/screen/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+    render() {
+        return (
+            <div id="header">
+                <span>BOOKS</span>
+            </div>
+        )
+    }
+}
+
+export default Nav
