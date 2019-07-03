9b492c87d186a0990c9a4545ba0d1dd587a59c18
zidni0192 <izedeledby@gmail.com>
2019-07-03 08:38:17 +0700

    Update maneh

--- src/screen/BookDetail.js
+++ src/screen/BookDetail.js
@@ -8,9 +8,14 @@ function BookDetail(props) {
     <div className="book-detail">
       <div>
         <ul>
-          <li><Link to="">Woyy</Link></li>
+          <li><Link to="">Edit</Link></li>
+          <li><Link to="">Delete</Link></li>
         </ul>
         <img src={data.image_url} alt={data.title}/>
+      </div>
+      <div className="content">
+      <p className="title">{data.title}</p>
+      <p className="date">{data.updated_at}</p>
       </div>
     </div>
   )
