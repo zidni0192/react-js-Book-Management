50c86ef3429c098cd4d8466fec056d10ef212889
zidni0192 <izedeledby@gmail.com>
2019-07-05 22:47:10 +0700

    fixing bug

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
     return (
       <div id="app">
         <Router>
-          <Redirect to="/book" />
+
           <Route exact path={"/book"} component={Nav} />
           <Route exact path={"/book"} render={()=><Search setSearch={this.setSearch}/>} />
           <Route exact path={"/book"} render={() => <List prop={this.state} search={this.state.search} showModal={this.showModal} />} />
